010ad53abdd8764175eed71fd829acd55a0bc440
MiguelGomez31 <48809299+MiguelGomez31@users.noreply.github.com>
2019-04-22 11:52:07 -0600

    Generador de contraseñas

---
+++ MiguelGomez.py
@@ -0,0 +1,12 @@
+from random import choice
+from string import ascii_letters , digits
+import os
+import sys
+
+cuantas = 1
+caracteres = ascii_letters + digits
+def generador(ab):
+    for i in range(ab):
+        final = ''.join([choice (caracteres) for i in range(4,16)])
+        print (final)
+generador(cuantas)
